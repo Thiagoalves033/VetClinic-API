43491ab77a9f493809a39c21f149050509f5b8bb
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2023-11-08 23:24:32 -0300

    feat: Add the controller functions to routes

--- src/routes/clinic.ts
+++ src/routes/clinic.ts
@@ -1,13 +1,20 @@
 import express from 'express';
+import {
+    getAllTutors,
+    createTutor,
+    updateTutor,
+    deleteTutor,
+    createPet,
+    updatePet,
+    deletePet,
+} from '../controllers/clinic'
 
 const router = express.Router()
 
-// Controller functions go here
-
-router.route('/tutors').get()
-router.route('/tutor').post()
-router.route('/tutor/:id').put().delete()
-router.route('/pet/:tutorId').post()
-router.route('/pet/:petId/tutor/:tutorId').put().delete()
+router.route('/tutors').get(getAllTutors)
+router.route('/tutor').post(createTutor)
+router.route('/tutor/:id').put(updateTutor).delete(deleteTutor)
+router.route('/pet/:tutorId').post(createPet)
+router.route('/pet/:petId/tutor/:tutorId').put(updatePet).delete(deletePet)
 
 export = router

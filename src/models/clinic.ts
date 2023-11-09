21409d41cb789fcc101684f22fb039b12530797a
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2023-11-09 20:51:29 -0300

    feat: Add a new schema and update ClinicSchema
    This commit does some changes to /models/clinic.ts: ClinicSchema
    was renamed to TutorSchema and PetSchema was created for pets, as well
    as interfaces for both of them.
    
    This modification was necessary because having the 'pets' property of
    TutorSchema as 'Array<Pets>' was incorrect, since 'pets' is an array of
    documents of type Pets.
    
    Some very basic validation was also added to some properties of
    TutorSchema.

--- src/models/clinic.ts
+++ src/models/clinic.ts
@@ -1,20 +1,44 @@
-import mongoose from "mongoose";
+import mongoose, {Document} from "mongoose";
 
 interface Pets {
-    name: String,
-    species: String,
-    carry: String,
-    weight: Number,
-    date_of_birth: String,
+    name: string;
+    species: string;
+    carry: string;
+    weight: number;
+    date_of_birth: string;
 }
 
-const ClinicSchema = new mongoose.Schema({
+interface Tutors extends Document {
+    name: string;
+    phone: string;
+    email: string;
+    date_of_birth: string;
+    zipCode: string;
+    pets: Pets[];
+}
+
+const PetSchema = new mongoose.Schema<Pets>({
     name: {type: String, required: true},
-    phone: {type: String, required: true},
-    email: {type: String, required: true},
+    species: {type: String, required: true},
+    carry: {type: String, required: true},
+    weight: {type: Number, required: true},
     date_of_birth: {type: String, required: true},
-    zipCode: {type: String, required: true},
-    pets: {type: Array<Pets>, default: {pets: 'None'}}
 })
 
-export = mongoose.model('Clinic', ClinicSchema)
+const Pet = mongoose.model('Pet', PetSchema)
+
+const TutorSchema = new mongoose.Schema<Tutors>({
+    name: {type: String, required: true},
+    phone: {type: String, required: true, minlength: 11, maxlength: 11},
+    email: {type: String, required: true},
+    date_of_birth: {type: String, required: true},
+    zipCode: {type: String, required: true, minlength: 8, maxlength: 8},
+    pets: {type: [PetSchema], default: []}
+})
+
+const Tutor = mongoose.model('Tutor', TutorSchema)
+
+export {
+    Pet,
+    Tutor,
+}
